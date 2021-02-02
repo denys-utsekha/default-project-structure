7e2cc55b61a1644bbdedeacf4269665a1106ee8e
Denys Utsekha <denysutsekha@gmail.com>
2021-02-02 15:25:47 +0200

    [ADD] - Default structure

---
+++ src/context/IntlContext/IntlContext.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const IntlContext = ({ children }) => {
+  return children;
+};
+
+export default IntlContext;
---
+++ src/language/index.js
@@ -0,0 +1,9 @@
+import ruMessages from "./locales/ru_RU.json";
+import enMessages from "./locales/en_US.json";
+
+const messages = {
+  ru: ruMessages,
+  en: enMessages,
+};
+
+export default messages;
